4da1b6d092c215b263abc2483d764f1fc2963f3a
skywalker96 <udayanga.2015253@iit.ac.lk>
2017-01-23 23:31:39 +0530

    retrive product info from the db

--- prodinfo.php
+++ prodinfo.php
@@ -9,6 +9,23 @@ $pagename="Product Information";
 $prodid=$_GET['u_prodid'];
 //echo "<p>Selected product Id: ".$prodid;
 
+//query the product table to retrieve the record for which the value of the product id
+//matches the product id of the product that was selected by the user
+$prodSQL="select prodId, prodName, prodPicName, 
+prodDescrip , prodPrice, prodQuantity from product
+where prodId=".$prodid ;
+//execute SQL query
+$exeprodSQL=mysqli_query($conn,$prodSQL) or die(mysqli_error());
+//create array of records & populate it with result of the execution of the SQL query
+$thearrayprod=mysqli_fetch_array($exeprodSQL);
+
+//display product name in capital letters
+//echo "<p><center><b>".strtoupper($thearrayprod['prodName'])."</b></center>";
+
+//call in the style sheet called ystylesheet.css to format the page as defined in the style sheet
+echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";
+
+
 
 //include head layout
 include("footfile.html");
