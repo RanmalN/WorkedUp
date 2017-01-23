f9c8ce0a10d6e8e161b285fc6876a765157d5900
skywalker96 <udayanga.2015253@iit.ac.lk>
2017-01-23 22:30:54 +0530

    create a product info page

---
+++ prodinfo.php
@@ -0,0 +1,15 @@
+﻿<?php
+session_start();
+
+//create a variable called $pagename which contains the actual name of the page
+include("db.php");
+$pagename="Product Information";
+//retrieve the product id passed from the previous page using the $_GET superglobal variable
+//store the value in a variable called $prodid
+$prodid=$_GET['u_prodid'];
+//echo "<p>Selected product Id: ".$prodid;
+
+
+//include head layout
+include("footfile.html");
+?>
