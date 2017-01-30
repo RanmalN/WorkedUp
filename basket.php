ddb8826f1ef5c65fc78ce63fc6e5723d7bec9798
skywalker96 <udayanga.2015253@iit.ac.lk>
2017-01-30 11:35:20 +0530

    add a session to store purchase products

--- basket.php
+++ basket.php
@@ -6,6 +6,17 @@ $pagename="Ordering Basket";
 //call in the style sheet called ystylesheet.css to format the page as defined in the style sheet
 echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";
 
+//display window title
+echo "<title>".$pagename."</title>";
+//include head layout
+include ("headfile.html");
+include ("detectlogin.php");
+echo "<p></p>";
+//display name of the page and some random text
+echo "<h2>".$pagename."</h2>";
+//@$prodNameAdd=$_POST['h_prodName'];
+//@$prodpriceAdd=$_POST['h_prodPrice'];
+
 
 
 
