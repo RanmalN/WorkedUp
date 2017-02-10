5707767c828d0f7b2e1a631c3eb3fcb0ebc79862
skywalker96 <udayanga.2015253@iit.ac.lk>
2017-02-10 17:40:16 +0530

    fix cleae basket

--- clearbasket.php
+++ clearbasket.php
@@ -1,6 +1,16 @@
 <?php
 session_start();
 
+//create a variable called $pagename which contains the actual name of the page
+$pagename="Template";
+
+//call in the style sheet called ystylesheet.css to format the page as defined in the style sheet
+echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";
+
+//display window title
+echo "<title>".$pagename."</title>";
+//include head layout
+include ("headfile.html");
 include ("detectlogin.php");
 
 echo "<p></p>";
